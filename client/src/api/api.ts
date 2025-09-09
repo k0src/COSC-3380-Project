f226088708704e32b264bf77c6897ead24d9b64e
k0 <k0src@null.net>
2025-09-09 13:19:54 -0500

    update env variables for client

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: `/api`,
+  baseURL: API_BASE_URL || "/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
