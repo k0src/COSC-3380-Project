65144b5476ad6449600f8a1c966aab643db6a25c
Ron Alex <ronmathewalx@gmail.com>
2025-10-15 03:20:38 -0500

    starting off with the artist page

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import * as Pages from "./pages";
 import HomePage from "./pages/HomePage/HomePage"
+import ArtistPage from "./pages/ArtistPage/ArtistPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={< HomePage />} />
       <Route path="/test" element={<Pages.TestPage />} />
+      <Route path="/artist" element={< ArtistPage />} />
     </Routes>
   );
 }
