5267e6e57b42505844d42f21b1639b9abf54adfe
Ron Alex <ronmathewalx@gmail.com>
2025-11-11 22:06:48 -0600

    Removing deprecated function

--- client/src/api/song.api.ts
+++ client/src/api/song.api.ts
@@ -9,22 +9,6 @@ import type {
 } from "@types";
 
 export const songApi = {
-  async getSongs(options?: {
-    includeAlbums?: boolean;
-    includeArtists?: boolean;
-    includeLikes?: boolean;
-    includeComments?: boolean;
-    orderByColumn?: string;
-    orderByDirection?: "ASC" | "DESC";
-    limit?: number;
-    offset?: number;
-  }) {
-    const response = await api.get<Song[]>(`/songs`, {
-      params: options,
-    });
-    return response.data;
-  },
-
   async getSongById(
     id: UUID,
     options?: {
