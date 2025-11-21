97d95e9a63be59b4b400173c521053648f998676
k0 <k0src@null.net>
2025-11-20 21:49:59 -0600

    button animations

--- client/src/pages/ArtistDashboard/ArtistDashboardManagePage/ArtistDashboardManagePage.tsx
+++ client/src/pages/ArtistDashboard/ArtistDashboardManagePage/ArtistDashboardManagePage.tsx
@@ -1,8 +1,22 @@
 import { memo } from "react";
+import { Helmet } from "react-helmet-async";
+import { useAuth } from "@contexts";
 import styles from "./ArtistDashboardManagePage.module.css";
 
 const ArtistDashboardManagePage: React.FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Helmet>
+        <title>Manage Content - CoogMusic</title>
+      </Helmet>
+
+      <div className={styles.manageLayout}>
+        <header className={styles.manageHeader}>
+          <span className={styles.manageTitle}>Manage Content</span>
+        </header>
+      </div>
+    </>
+  );
 };
 
 export default memo(ArtistDashboardManagePage);
